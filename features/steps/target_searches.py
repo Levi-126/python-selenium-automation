1a7e0f40064f6cdbeac4e9d5c45ad680779b7de5
Levi126 <levi126@gmail.com>
2026-05-07 23:56:31 -0400

    Target Log In

--- features/steps/target_searches.py
+++ features/steps/target_searches.py
@@ -2,6 +2,7 @@ from selenium.webdriver.common.by import By
 from behave import given, when, then
 from time import sleep
 
+
 @given('Open Target Webpage')
 def open_target(context):
     context.driver.get('https://www.target.com/')
